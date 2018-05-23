521c2c444e17ec141f35a3cff970bfd13f1a753e
clarencedglee <clarencedglee@gmail.com>
2018-05-23 22:25:16 +0100

    adding composition ideas

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Composer from "react-composer";
 import { compose } from "recompose";
 
 import { createBait, queryBaits, queryRates } from "./gql";
-import { QueryProvider, MutationProvider } from "./utils";
+import { QueryProvider, MutationProvider, unwrapGqlResults } from "./utils";
 import { WithLoading } from "./loading.component";
 import { WithError } from "./error-message.component";
 import Display from "./display.component";
@@ -44,6 +44,77 @@ const RatesAndBaits = () => (
     }}
   </Composer>
 );
+
+const RAB = composeComponents(
+  [Query, { query: queryRates }],
+  [Query, { query: queryBaits }],
+  [Mutation, { mutation: createBait }]
+)(([rates, baits, createBait]) => {
+  const { createBaitFn, createBaitObj } = createBait;
+  if (rates.loading || baits.loading) return "Loading";
+  if (rates.error || baits.error) return "Error";
+  return (
+    <F>
+      <p>
+        {unwrapGqlResults(rates, "rates", 0).currency}
+        {unwrapGqlResults(rates, "rates", 0).color}
+      </p>
+      <button onClick={() => createBaitFn({ variables: { name: "foo" } })}>
+        {createBaitObj.loading ? "Adding" : "Add"} Bait
+      </button>
+    </F>
+  );
+});
+
+const apolloQuery = query => [Query, { query }];
+const apolloMutation = mutation => [Mutation, { mutation }];
+
+const RAB = composeComponents(
+  apolloQuery(queryRates),
+  apolloQuery(queryBaits),
+  apolloMutation(createBait)
+)(([rates, baits, createBait]) => {
+  const { createBaitFn, createBaitObj } = createBait;
+  if (rates.loading || baits.loading) return "Loading";
+  if (rates.error || baits.error) return "Error";
+  return (
+    <F>
+      <p>
+        {unwrapGqlResults(rates, "rates", 0).currency}
+        {unwrapGqlResults(rates, "rates", 0).color}
+      </p>
+      <button onClick={() => createBaitFn({ variables: { name: "foo" } })}>
+        {createBaitObj.loading ? "Adding" : "Add"} Bait
+      </button>
+    </F>
+  );
+});
+
+Gql = {
+  Mutation: 0,
+  Query: 1
+};
+
+const RAB = composeGql(
+  [Gql.Query, queryRates],
+  [Gql.Query, queryBaits],
+  [Gql.Mutation, createBait]
+)(([rates, baits, createBait]) => {
+  const { createBaitFn, createBaitObj } = createBait;
+  if (rates.loading || baits.loading) return "Loading";
+  if (rates.error || baits.error) return "Error";
+  return (
+    <F>
+      <p>
+        {unwrapGqlResults(rates, "rates", 0).currency}
+        {unwrapGqlResults(rates, "rates", 0).color}
+      </p>
+      <button onClick={() => createBaitFn({ variables: { name: "foo" } })}>
+        {createBaitObj.loading ? "Adding" : "Add"} Bait
+      </button>
+    </F>
+  );
+});
 
 class App extends Component {
   render() {
